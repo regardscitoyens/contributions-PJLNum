661fa96a3f4c781016adfaa4a3bd600915ba4117
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2015-12-12 18:32:28 +0100

    web view

---
+++ web/js/networks.js
@@ -0,0 +1,63 @@
+(function(ns){
+
+  ns.sigma = undefined;
+
+  ns.downloadGraph = function(){
+    $.get('data/networks/users_supporters.json').then(function(data){
+      console.log(data)
+      if (ns.sigma) ns.sigma.kill();
+      ns.sigma = new sigma({
+        container: 'graph',
+        settings: {
+          labelThreshold: 4,
+          singleHover: true,
+          minNodeSize: 0.1,
+          edgeColor: "default",
+          borderSize: 1
+        }
+      }).configForceAtlas2({
+        adjustSizes: true,
+        scalingRatio: 10,
+        strongGravityMode: true,
+        gravity: 0.1,
+        slowDown: 15,
+
+      });
+      data["nodes"].forEach(function(n){
+        ns.sigma.graph.addNode({
+          id: n.id,
+          label: n.label,
+          type: n.attributes.type,
+          votes: n.attributes.total_votes,
+          contributions: n.attributes.total_contributions,
+          x: n.x*10,
+          y: -n.y*10,
+          size: Math.pow(n.size, 3),
+          color: n.color
+        });
+      });
+      data["edges"].forEach(function(e){
+        ns.sigma.graph.addEdge({
+          "id": e.id,
+          "source": e.source,
+          "target": e.target,
+          "weight": e.attributes.Weight
+        });
+      });
+      ns.sigma.refresh();
+    });
+  };
+
+  ns.setResponsive = function(){
+    $('#graph').width($(window).width() - 50)
+               .height($(window).height() - 50);
+  };
+
+  $(window).resize(ns.setResponsive);
+
+  $(document).ready(function(){
+    ns.setResponsive();
+    ns.downloadGraph();
+  });
+
+})(window.networksPJLNum = window.networksPJLNum || {});
