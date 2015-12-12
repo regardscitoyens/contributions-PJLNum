81ea597bf7cd6456c68ebcf43aa4a1fd389cc62e
Tangui Morlier <tangui@tangui.eu.org>
2015-12-12 17:09:57 +0100

    Supprime le contenu des données liées à des comptes qui n'ont pas fait de propositions ou d'amendements

--- anon/anonymise.py
+++ anon/anonymise.py
@@ -6,6 +6,16 @@ import md5
 sans_identifiant = 0
 salt = str(random.random())
 
+has_to_be_public = {}
+
+with open(sys.argv[1], 'r') as csvfileread:
+    csvreader = csv.reader(csvfileread, delimiter=',', quotechar='"')
+    for row in csvreader:
+        if (row[3] == ""):
+            row[3] = "Citoyen"
+        if (row[7] == "Proposition") or (row[7] == "Amendement") or (row[3] != "Citoyen"):
+            has_to_be_public[row[2]] = True
+
 with open(sys.argv[1], 'r') as csvfileread:
     with open(sys.argv[2], 'w') as csvfilewrit:
         csvreader = csv.reader(csvfileread, delimiter=',', quotechar='"')
@@ -25,6 +35,11 @@ with open(sys.argv[1], 'r') as csvfileread:
             idmd5 = md5.new(row[1]+row[2]+salt).hexdigest()
             row.append(idmd5)
             row.append(int(idmd5[:8], 16))
+            if not (row[2] in has_to_be_public):
+                row[1] = ""
+                row[2] = ""
+            if (row[3] == ""):
+                row[3] = "Citoyen"
             csvwriter.writerow(row)
 
 
